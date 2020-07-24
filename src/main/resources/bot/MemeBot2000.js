1b8c27d9df5dd44f82a1ff11fa129f7e3a2f1dd0
JJMarsz <jjmarsz2@illinois.edu>
2020-07-24 11:23:16 -0500

    fixing zabory's face's branch issues

--- src/main/resources/bot/MemeBot2000.js
+++ src/main/resources/bot/MemeBot2000.js
@@ -1,31 +1,25 @@
 var Discord = require('discord.js');
 var logger = require('winston');
 var auth = require('./auth.json');
-// // Configure logger settings
-// logger.remove(logger.transports.Console);
-// logger.add(logger.transports.Console, {
-//     colorize: true
-// // });
-// logger.level = 'debug';
-// Initialize Discord Bot
+
 var bot = new Discord.Client();
-bot.on('ready', function (evt) {
-  //   var lastMsg = bot.channels.get(auth.channel).fetchMessages()
-  // .then(messages => `${messages.filter(m => m.author.id === '674409301409923093').first().delete()} messages`);
-  // console.log(lastMsg);
+
+bot.on('ready', () => {
+	bot.user.setActivity("Someone get this man a meme")
 });
-bot.on('message', message => {
-	console.log(message.content);
-    if (message.content.length > 4 &&
-    	message.content.substring(0, 4) == '!RIP'){
-    	var name = message.content.substring(4);
 
-    	var msg = 'RIP ' + name
-    	console.log(msg);
-        message.delete();
-        bot.channels.get(auth.channel).send(msg);
+bot.on('message', data => {
+	user = data.author.username
+	if(user != 'MemeBot2000'){
+		channel = data.channel.id
+		if(data.attachments.size > 0){
+			url = data.attachments.array()[0].url
+			tags = data.content
+			console.log(user + "," + channel + "," + url + "," + tags)
+		}else{
+			data.reply('Please attach a meme next time')
+		}
 	}
-
 });
 
 bot.login(auth.token)
