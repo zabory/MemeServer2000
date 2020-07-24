cb4663596b46afd9847d8a9ba7e95516fe8036d9
zabory <benshabowski@gmail.com>
2020-07-23 23:28:33 -0500

    updating meme bot

--- src/main/resources/MemeBot2000.js
+++ src/main/resources/MemeBot2000.js
@@ -9,7 +9,17 @@ bot.on('ready', () => {
 });
 
 bot.on('message', data => {
-	console.log('heckin log dude')
+	user = data.author.username
+	if(user != 'MemeBot2000'){
+		channel = data.channel.id
+		if(data.attachments.size > 0){
+			url = data.attachments.array()[0].url
+			tags = data.content
+			console.log(user + "," + channel + "," + url + "," + tags)
+		}else{
+			data.reply('Please attach a meme next time')
+		}
+	}
 });
 
 bot.login(auth.token)
