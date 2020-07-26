22914d604da9ec9993b6053a420095931bfa4506
JJMarsz2 <jjmarsz2@illinois.edu>
2020-07-25 21:59:30 -0500

    comments

--- src/main/java/app/MemeServer2000.java
+++ src/main/java/app/MemeServer2000.java
@@ -1,7 +1,17 @@
 package app;
 
 public class MemeServer2000 {
-	
+
+	//this class will act as the controller
+	// create a bot and memedb
+	// create child to monitor approval Q
+		// watch the Q, if a meme appears, send it to the approval channel
+		// on response, controller will inform DB and this child
+	// read input buffer from bot
+	// use that to spawn children
+	//children perform tasks to populate output buffer and approval Q
+	//parallel issues
+
 	public static void main(String[] args) {
 		
 		
