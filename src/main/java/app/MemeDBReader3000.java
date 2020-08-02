1cbe2e210985921179189870fdfa0f5126477892
JJMarsz2 <jjmarsz2@illinois.edu>
2020-08-01 19:47:04 -0500

    #25 reorganized the db reader

--- src/main/java/app/MemeDBReader3000.java
+++ src/main/java/app/MemeDBReader3000.java
@@ -31,54 +31,43 @@ public class MemeDBReader3000 extends Thread{
     public void run(){
         MemeDBMsg2000 msg;
         Integer lastID = null;
-        boolean sendMsg;
 
         while(true) {
-            MemeBotMsg2000 newMsg = new MemeBotMsg2000();
-            sendMsg = true;
             try {
                 msg = dbOutputQ.take();
                 switch(msg.getType()){
                     case REPLENISH_Q:
                         logger.println("Meme ID of " + msg.getId() + " was put into pending approval.");
                         approveQ.put(msg.getId());
-                        sendMsg = false;
                         break;
 
                     case ALL_TAGS:
                         logger.println("Sending all tags to bot");
-                        newMsg.setCommand("sendAllTags");
                         String tagList = "";
                         for(String e : msg.getTags()) {
                             tagList += e + ",";
                         }
-                        newMsg.setBody(tagList);
+                        botInputQ.put(new MemeBotMsg2000().command("sendAllTags").body(tagList));
                         break;
 
                     case SUBMIT_ACK:
                         if(msg.getId() != null){
                             logger.println("Received ACK for cached meme of ID " + msg.getId());
                             approveQ.put(msg.getId());
-                            newMsg.setChannelID(approveQ.size());
                         }
                         else
                             logger.println("Received ACK for meme submitted by " + msg.getUsername());
-                        newMsg.setCommand("sendToUser");
-                        newMsg.setUser(msg.getUsername());
-                        newMsg.setBody(msg.getMessage());
+                        botInputQ.put(new MemeBotMsg2000().command("sendToUser").body(msg.getMessage()).user(msg.getUsername()));
                         break;
 
                     case APPROVE_MEME:
                         logger.println("Sending meme ID " + msg.getId() + " to be approved");
-                        newMsg.setCommand("sendToQueue");
-                        newMsg.setBody(msg.getLink());
-                        newMsg.setChannelID(approvalChannelID);
                         String tags = "**Tags**:\n";
                         for(int i=0;i<msg.getTags().size();i++)
                             tags += i + ": " + msg.getTags().get(i) + "\n";
 
-                        MemeBotMsg2000 approveMsgHeader = new MemeBotMsg2000().channelID(approvalChannelID).body("**Queue count**: " + (approveQ.size()-1) + "\n" + tags).command("sendToChannel");
-                        botInputQ.put(approveMsgHeader);
+                        botInputQ.put(new MemeBotMsg2000().channelID(approvalChannelID).body("**Queue count**: " + (approveQ.size()-1) + "\n" + tags).command("sendToChannel"));
+                        botInputQ.put(new MemeBotMsg2000().command("sendToQueue").body(msg.getLink()).channelID(approvalChannelID));
                         break;
 
                     case CURATE_RESULT:
@@ -86,31 +75,21 @@ public class MemeDBReader3000 extends Thread{
                         approveQ.take();
                         botInputQ.put(new MemeBotMsg2000().command("clearQueue"));
                         botInputQ.put(new MemeBotMsg2000().command("sendToUser").body(msg.getLink()).user(msg.getUsername()));
-
-                        newMsg.setCommand("sendToUser");
-                        newMsg.setUser(msg.getUsername());
-                        newMsg.setBody(msg.getMessage());
+                        botInputQ.put(new MemeBotMsg2000().command("sendToUser").body(msg.getMessage()).user(msg.getUsername()));
                         break;
 
                     case MEME:
                         logger.println("Sending meme to " + msg.getChannelID() + ", requested by " + msg.getUsername());
-                        newMsg.setCommand("sendToChannel");
-                        newMsg.setUser(msg.getUsername());
-                        newMsg.setBody(msg.getLink());
-                        newMsg.setChannelID(msg.getChannelID());
+                        botInputQ.put(new MemeBotMsg2000().command("sendToChannel").body(msg.getLink()).user(msg.getUsername()).channelID(msg.getChannelID()));
                         break;
 
                     case ERROR:
-                        newMsg.setCommand("sendToUser");
-                        newMsg.setUser(msg.getUsername());
-                        newMsg.setBody(msg.getMessage() + msg.getTags());
+                        botInputQ.put(new MemeBotMsg2000().command("sendToUser").body(msg.getMessage() + msg.getTags()).user(msg.getUsername()));
                         break;
 
                     default:
                         logger.println("Main cannot handle a message of type: " + msg.getType().toString() + " as an output of the DB");
                 }
-                if(sendMsg)
-                    botInputQ.put(newMsg);
 
                 // check approveQ for a new ID
                 if(!approveQ.isEmpty() && approveQ.peek() != lastID){
